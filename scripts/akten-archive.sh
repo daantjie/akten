a8507dd156bb3f3b02093cbab4b019b3aac15601
Daniel Oosthuizen <danieltheexperimenter@gmail.com>
2016-02-14 14:58:56 +0000

    Add initial akten-archive.sh
    Just boilerplate so far.

---
+++ scripts/akten-archive.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+## Functions
+usage () {
+    echo "\
+
+        akten-archive [options]
+
+wip
+"
+}
+
+## Defaults
+
+
+## Parse arguments
+while [ $# != 0 ]; do
+    case "$1" in
+	*)
+	    ## wip
+	    ;;
+    esac
+    shift
+done
